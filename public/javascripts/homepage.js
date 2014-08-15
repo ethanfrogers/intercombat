30ac923528af86256e09b6bc95e6a9fac577f9ac
Ethan Rogers <ethan@supplyhog.com>
2014-08-15 17:49:49 -0400

    streaming

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -1,7 +1,10 @@
+
 $('document').ready(function(){
     var socket = io.connect('http://localhost:3000');
     socket.on('ping-back' ,function(){
        alert('Connected to Socket.io');
     });
+
+    var stream = ss.createStream();
 });
 

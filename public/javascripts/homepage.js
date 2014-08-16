8c45fc27cc281ccaf818021ec226398911e42040
Ethan Rogers <ethan@supplyhog.com>
2014-08-15 23:00:24 -0400

    fix

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -7,15 +7,15 @@ $('document').ready(function(){
     var stream = ss.createStream();
     socket.on('new-audio', function(data){
         $('audio').attr('type', 'audio/mpeg').attr('src',data.path).trigger('play');
+        stream = ss.createStream();
     });
     socket.on('message-sent', function(){
         alert('Your message has been sent');
     });
 
     document.uploadToIntercom = function(blob){
-        console.log(blob);
-            ss(socket).emit('profile-image', stream, {name: 'testfile.wav'});
-            ss.createBlobReadStream(blob).pipe(stream);
+        ss(socket).emit('profile-image', stream, {name: 'testfile.wav'});
+        ss.createBlobReadStream(blob).pipe(stream);
     }
 
 

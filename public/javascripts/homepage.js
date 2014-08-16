68cec13f20a47062c42a4ad5c36ea3e408166114
Ethan Rogers <ethan@supplyhog.com>
2014-08-16 10:18:11 -0400

    update listing when someone sends a new message because real time

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -8,29 +8,35 @@ $('document').ready(function(){
     socket.on('new-audio', function(data){
         $('audio').attr('src','/messages/url/' + data.path).trigger('play');
         stream = ss.createStream();
+        updateMessageListing();
     });
     socket.on('message-sent', function(){
         alert('Your message has been sent');
     });
 
+    updateMessageListing();
     document.uploadToIntercom = function(blob){
         ss(socket).emit('profile-image', stream, {name: 'testfile.wav'});
         ss.createBlobReadStream(blob).pipe(stream);
     }
 
-    $.get('http://localhost:3000/listing', function(response){
-        if(response.success == 1){
-            $.each(response.files, function(index, name){
-                $(".file-listings").append('<li class="item" data-filename="' + name + '">' + name + '</li>');
-            });
-            $('.item').on('click', function(){
-                $('audio').attr('src','/messages/url/' + $(this).data('filename')).trigger('play');
-            });
 
-        } else {
-            //there was an error
-        }
-    });
+    function updateMessageListing(){
+        $.get('http://localhost:3000/listing', function(response){
+            $('.file-listings').html('');
+            if(response.success == 1){
+                $.each(response.files, function(index, name){
+                    $(".file-listings").append('<li class="item" data-filename="' + name + '">' + name + '</li>');
+                });
+                $('.item').on('click', function(){
+                    $('audio').attr('src','/messages/url/' + $(this).data('filename')).trigger('play');
+                });
+
+            } else {
+                $(".file-listings").append('<li>No Messages to Display</li>');
+            }
+        });
+    }
 
 
 

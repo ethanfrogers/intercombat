c08ff09e0449d678d1cc529d4250462c6b2df672
Ethan Rogers <ethan@supplyhog.com>
2014-08-15 20:58:42 -0400

    upload audio file write file then return and play file

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -1,10 +1,27 @@
 
 $('document').ready(function(){
     var socket = io.connect('http://localhost:3000');
-    socket.on('ping-back' ,function(){
-       alert('Connected to Socket.io');
+
+    $("#file").on('change',function(e){
+        console.log('file change');
+        var file = e.target.files[0];
+        var stream = ss.createStream();
+
+        // upload a file to the server.
+        console.log(file);
+        ss(socket).emit('profile-image', stream, {size: file.size, name: file.name});
+        ss.createBlobReadStream(file).pipe(stream);
+
+        socket.on('new-audio', function(data){
+            console.log(data);
+            $('audio').attr('type', 'audio/mpeg').attr('src',data.path);
+        });
     });
 
-    var stream = ss.createStream();
+
+
+
+
+
 });
 
